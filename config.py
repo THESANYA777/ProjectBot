af57ef6b0df5243b626f8c81740b7310161143df
$ANYA <ghelen7841@gmail.com>
2024-04-23 20:31:44 +0300

    crypto

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = ''
+
+bitcoin = 'https://coinmarketcap.com/currencies/bitcoin/'
+usdt = 'https://coinmarketcap.com/currencies/tether/'
+sol = 'https://coinmarketcap.com/currencies/solana/'
+ton = 'https://coinmarketcap.com/currencies/toncoin/'
+matic = 'https://coinmarketcap.com/currencies/polygon/'
+near = 'https://coinmarketcap.com/currencies/near-protocol/'
+atom = 'https://coinmarketcap.com/currencies/cosmos/'
