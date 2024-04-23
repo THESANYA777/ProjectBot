d1df808971b9ae32221da23d2c28e8c119b964af
$ANYA <ghelen7841@gmail.com>
2024-04-23 23:32:01 +0300

    ctrl alt l db

--- core/db/db.py
+++ core/db/db.py
@@ -2,17 +2,21 @@ import aiosqlite
 
 DATABASE_FILE = 'core/db/db.db'
 
+
 # запуск бд (создание таблицы с пользователями)
 async def init_db() -> None:
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        await db.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER UNIQUE)')
+        await db.execute(
+            'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER UNIQUE)')
         await db.commit()
+
 
 # добавление пользователя в бд
 async def add_user(telegram_id: int) -> None:
     async with aiosqlite.connect(DATABASE_FILE) as db:
         await db.execute('INSERT OR IGNORE INTO users (telegram_id) VALUES (?)', (telegram_id,))
         await db.commit()
+
 
 # получение всех пользователей
 async def get_users():
